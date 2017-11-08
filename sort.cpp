7a5e773fdfe50d3dbd627bbb78a0798a535cabe9
kasiamroz <32893277+kasiamroz@users.noreply.github.com>
2017-11-08 09:10:29 +0100

    Add files via upload

--- sort.cpp
+++ sort.cpp
@@ -1,12 +1,22 @@
+//Sortowanie bąbelkowe z warunkiem stopu
+
 #include <iostream>
 using namespace std;
 
 void sortowanie_babelkowe(int tab[],int n)
 {
+  bool isSorted;
   for(int i=0;i<n;i++)
+  {
+    isSorted=true;  
     for(int j=1;j<n-i;j++)
-    if(tab[j-1]>tab[j])
+    {
+	  if(tab[j-1]>tab[j])
       swap(tab[j-1], tab[j]);
+      isSorted=false;
+      if(true==isSorted) break;
+    }
+  }
 }
 
 int main()
